0c4328cb8b3951feb581a3ed901a643f3a55c027
rickiewsz <1105122704@qq.com>
2016-04-15 21:29:59 +0800

    manager

--- app/src/main/java/com/example/wangzhengkui/preferencefile/manager/ConfigureManager.java
+++ app/src/main/java/com/example/wangzhengkui/preferencefile/manager/ConfigureManager.java
@@ -1,21 +1,20 @@
 package com.example.wangzhengkui.preferencefile.manager;
 
-import android.content.Context;
-import android.util.SparseArray;
 
-import com.example.wangzhengkui.preferencefile.KeyValue;
+import android.text.TextUtils;
+
+import com.example.wangzhengkui.preferencefile.cache.PreferencesCacheManager;
+import com.example.wangzhengkui.preferencefile.cache.PreferencesCacheManager.EditorImpl;
 import com.example.wangzhengkui.preferencefile.entity.ConfigureEntity;
 import com.example.wangzhengkui.preferencefile.entity.ConfigureScreenEntity;
 import com.example.wangzhengkui.preferencefile.entity.Constants;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * @author Administrator on 2016-04-06 14:42
@@ -28,26 +27,77 @@ public class ConfigureManager {
     public static final String VALUE = "value";
     public static final String ENTRY = "entry";
     public static final String DEFAULT_VALUE = "defaultValue";
-
+    public static final String VERSION = "version";
+    public static final String EMPTY = "  ";
+    public static final String RELEASE = "0";
 
     private static JSONObject dataJson;
 
     public static void init() {
-        //根据版本号，是否删除缓存
+        String oldVersion = readCacheValue(VERSION);
+        if (EMPTY.equals(oldVersion)) {
+            writeCacheValue(VERSION, Constants.version);
+        } else if (Constants.version.compareTo(oldVersion) > 0) {
+            //配置版本号高于原来的版本号 清除缓存
+            clearCache();
+            writeCacheValue(VERSION, Constants.version);
+        }
     }
+
+    /***
+     * 将json字符串解析存到缓存
+     */
+    public static String parseData(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        HashMap<String, String> maps = DataParseTools.parseJson();
+        writeCacheValueNotExist(maps);
+        return maps.get(key);
+    }
+
+    /***
+     * 保存还没有记录的key value，如果已经存在 则不保存
+     */
+    private static void writeCacheValueNotExist(HashMap<String, String> hashMap) {
+        if (hashMap == null || hashMap.isEmpty()){
+            return;
+        }
+        PreferencesCacheManager mgr = PreferencesCacheManager.getInstance();
+        EditorImpl editor = mgr.edit();
+        Set<Entry<String, String>> sets = hashMap.entrySet();
+        for(Entry<String, String> set : sets) {
+            String key = set.getKey();
+            String value = set.getValue();
+            if (TextUtils.isEmpty(key) || TextUtils.isEmpty(value) || isCacheValue(key)) {
+                continue;
+            }
+            editor.putString(key, value);
+        }
+        editor.commit();
+    }
+
     /**
      *
      * @param key
      * @return
      */
-    public static Object getValue(String key) {
-        //release
-//        return key;
+    public static String getValue(String key) {
+
+        if (TextUtils.isEmpty(key)) {
+            return null;
+        }
+        //realse
+        if (TextUtils.equals(Constants.mode, RELEASE)) {
+            return key;
+        }
         //debug
-        if (isCacheValue(key)) {
-            return readCacheValue(key);
-        } else {
-            return "defalutValue";
+        String cacheValue = readCacheValue(key);
+        //有缓存值 直接返回
+        if (!EMPTY.equals(cacheValue)) {
+            return cacheValue;
+        }else {   //否则 返回默认值
+           return parseData(key);
         }
     }
 
@@ -56,21 +106,42 @@ public class ConfigureManager {
      * @param key
      * @return
      */
-    public static Object readCacheValue(String key) {
-
-        return null;
+    public static String readCacheValue(String key) {
+        if (TextUtils.isEmpty(key)) {
+            return EMPTY;
+        }
+        PreferencesCacheManager mgr = PreferencesCacheManager.getInstance();
+        return mgr.getString(key, EMPTY);
     }
-    public static boolean isCacheValue(String key) {
 
-        return false;
+    /**
+     * 清除所有缓存
+     */
+    private static void clearCache() {
+        PreferencesCacheManager mgr = PreferencesCacheManager.getInstance();
+        EditorImpl editor = mgr.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    /**
+     * 缓存是否有记录该Key相对于的value值
+     * @param key
+     * @return
+     */
+    public static boolean isCacheValue(String key) {
+        return EMPTY.equals(readCacheValue(key)) ? false : true;
     }
 
     /**
      * 写缓存
      * @param key
      */
-    public static void writeCacheValue(String key,String value) {
-
+    public static void writeCacheValue(String key, String value) {
+        PreferencesCacheManager mgr = PreferencesCacheManager.getInstance();
+        EditorImpl editor = mgr.edit();
+        editor.putString(key, value);
+        editor.commit();
     }
 
 
@@ -87,42 +158,42 @@ public class ConfigureManager {
         return null;
     }
 
-
+    /**
+     * 保存hashmap记录的key value值到缓存
+     * @param hashMap
+     */
+    public static void writeCacheValue(HashMap<String, String> hashMap) {
+        if (hashMap == null || hashMap.isEmpty()){
+            return;
+        }
+        PreferencesCacheManager mgr = PreferencesCacheManager.getInstance();
+        EditorImpl editor = mgr.edit();
+        Set<Entry<String, String>> sets = hashMap.entrySet();
+        for(Entry<String, String> set : sets) {
+            String key = set.getKey();
+            String value = set.getValue();
+            if (TextUtils.isEmpty(key) || TextUtils.isEmpty(value)) {
+                continue;
+            }
+            editor.putString(key, value);
+        }
+        editor.commit();
+    }
     /**
      * 返回key所对应的屏幕信息
      * @param key
      */
-    public static void getScreen(String key) {
-
-        /*for (int i = 0; i < 10; i++) {
-            if (!isCacheValue(key)) {
-                value = defalutValue;
-                writeCacheValue(key, value);
-            } else {
-                value = readCacheValue(key);
-            }
-        }*/
-    }
-
-
-    /**
-     * 更新缓存和
-     * @param key
-     * @param value
-     */
-    public static void update(String key, String value) {
-//        "apn"=value;
-//        Constants.apn = "cmwap";
-
+    public static ConfigureEntity getScreen(String key) {
+        return DataParseTools.getScreen(key);
     }
 
 
     /**
      * 重置缓存
      * @param entity
-     * @param restoreChild 是否重置子页
+     * @param restoreChild 是否同时重置子页
      */
     public static void restore(ConfigureEntity entity,boolean restoreChild) {
-
+        
     }
 }
--- app/src/main/java/com/example/wangzhengkui/preferencefile/manager/DataParseTools.java
+++ app/src/main/java/com/example/wangzhengkui/preferencefile/manager/DataParseTools.java
@@ -1,14 +1,18 @@
 package com.example.wangzhengkui.preferencefile.manager;
 
+import android.text.TextUtils;
+
 import com.example.wangzhengkui.preferencefile.entity.ConfigureEntity;
 import com.example.wangzhengkui.preferencefile.entity.ConfigureListEntity;
 import com.example.wangzhengkui.preferencefile.entity.ConfigureScreenEntity;
 import com.example.wangzhengkui.preferencefile.entity.ConfigureType;
+import com.example.wangzhengkui.preferencefile.entity.Constants;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -19,44 +23,42 @@ public class DataParseTools {
     public static ConfigureEntity parseData(String key, JSONObject jsonObject) {
         ConfigureEntity entity = null;
         //读取第一屏的数据
-        if (key == null) {
-            ConfigureScreenEntity screenEntity = new ConfigureScreenEntity();
-            Iterator<String> keys = jsonObject.keys();
-            LinkedList<ConfigureEntity> linkedList = new LinkedList<>();
-            //读取第一屏screen的key;
-            while (keys.hasNext()) {
-                key = keys.next();
-                screenEntity.setKey(key);
-            }
-
-            try {
+        Iterator<String> keys = jsonObject.keys();
+        ConfigureScreenEntity screenEntity = new ConfigureScreenEntity();
+        LinkedList<ConfigureEntity> linkedList = new LinkedList<>();
+        try {
+            if (key == null) {
+                //读取第一屏screen的key;
+                while (keys.hasNext()) {
+                    key = keys.next();
+                    screenEntity.setKey(key);
+                }
                 //读取第一屏所对应的json
                 jsonObject = jsonObject.getJSONObject(key);
-                keys = jsonObject.keys();
-                while (keys.hasNext()) {
-                    //每个item中，type是必须且肯定存在
-                    String childKey = keys.next();
-                    if ("type".equals(childKey)) {
-                        screenEntity.setType(jsonObject.optString(childKey));
-                    } else if ("value".equals(childKey)) {
-                        screenEntity.setValue(jsonObject.optString(childKey));
-                    } else if ("summary".equals(childKey)) {
-                        screenEntity.setSummary(jsonObject.optString(childKey));
-                    } else if ("title".equals(childKey)) {
-                        screenEntity.setTitle(jsonObject.optString(childKey));
-                    } else {
-                        JSONObject object = jsonObject.getJSONObject(childKey);
-                        ConfigureEntity entity1 = parseEntity(childKey, object);
-                        linkedList.add(entity1);
-                    }
-                }
-                screenEntity.setItemList(linkedList);
-                return screenEntity;
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
+            keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                //每个item中，type是必须且肯定存在
+                String childKey = keys.next();
+                if ("type".equals(childKey)) {
+                    screenEntity.setType(jsonObject.optString(childKey));
+                } else if ("value".equals(childKey)) {
+                    screenEntity.setValue(jsonObject.optString(childKey));
+                } else if ("summary".equals(childKey)) {
+                    screenEntity.setSummary(jsonObject.optString(childKey));
+                } else if ("title".equals(childKey)) {
+                    screenEntity.setTitle(jsonObject.optString(childKey));
+                } else {
+                    JSONObject object = jsonObject.getJSONObject(childKey);
+                    ConfigureEntity entity1 = parseEntity(childKey, object);
+                    linkedList.add(entity1);
+                }
+            }
+            screenEntity.setItemList(linkedList);
+            return screenEntity;
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
         return entity;
     }
 
@@ -66,6 +68,7 @@ public class DataParseTools {
         switch (type) {
             case ConfigureType.SCREEN:
                 entity = new ConfigureScreenEntity();
+
                 break;
             case ConfigureType.CATEGORY:
                 entity = new ConfigureEntity();
@@ -77,7 +80,7 @@ public class DataParseTools {
                     array = jsonObject.getJSONArray(ConfigureManager.ENTRY);
                     CharSequence[] entrys = new CharSequence[array.length()];
                     for (int i = 0; i < array.length(); i++) {
-                        entrys[i] = (CharSequence)array.get(i);
+                        entrys[i] = (CharSequence) array.get(i);
                     }
                     ((ConfigureListEntity) entity).setEntry(entrys);
                 } catch (JSONException e) {
@@ -101,7 +104,278 @@ public class DataParseTools {
         entity.setSummary(jsonObject.optString(ConfigureManager.SUMMARY));
         entity.setDefaultValue(jsonObject.opt(ConfigureManager.DEFAULT_VALUE));
         entity.setValue(jsonObject.opt(ConfigureManager.DEFAULT_VALUE));
-
         return entity;
     }
+
+
+
+   /* public static ConfigureEntity getScreenParseData(String key) {
+        ConfigureEntity entity = null;
+        //读取第一屏的数据
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(Constants.jsonDate);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (jsonObject == null) {
+            return null;
+        }
+        Iterator<String> keys = jsonObject.keys();
+        ConfigureScreenEntity screenEntity = new ConfigureScreenEntity();
+        LinkedList<ConfigureEntity> linkedList = new LinkedList<>();
+        boolean hadFinded = false;
+        try {
+            if (key == null) {
+                //读取第一屏screen的key;
+                while (keys.hasNext()) {
+                    key = keys.next();
+                    screenEntity.setKey(key);
+                }
+                //读取第一屏所对应的json
+                jsonObject = jsonObject.getJSONObject(key);
+                hadFinded = true;
+            }
+            keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                //每个item中，type是必须且肯定存在
+                String childKey = keys.next();
+                if ("type".equals(childKey)) {
+                    screenEntity.setType(jsonObject.optString(childKey));
+                } else if ("value".equals(childKey)) {
+                    screenEntity.setValue(jsonObject.optString(childKey));
+                } else if ("summary".equals(childKey)) {
+                    screenEntity.setSummary(jsonObject.optString(childKey));
+                } else if ("title".equals(childKey)) {
+                    screenEntity.setTitle(jsonObject.optString(childKey));
+                } else {
+                    JSONObject object = jsonObject.getJSONObject(childKey);
+                    String type = jsonObject.optString(ConfigureManager.TYPE);
+                    if (!hadFinded && TextUtils.equals(ConfigureType.SCREEN, type) && TextUtils.equals(childKey, key)) {
+
+                    }
+
+                    switch (type) {
+                        case ConfigureType.SCREEN:
+                            parseScreenData(key, jsonObject, hashMap);
+                            break;
+                    }
+                    ConfigureEntity entity1 = parseEntity(childKey, object);
+                    linkedList.add(entity1);
+                }
+            }
+            screenEntity.setItemList(linkedList);
+            return screenEntity;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return entity;
+    }
+*/
+
+    /***
+     * 解析json 得到所有的key value值
+     * @return
+     */
+    public static HashMap<String, String> parseJson() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        JSONObject object = null;
+        try {
+            object = new JSONObject(Constants.jsonDate);
+            parseScreenData(null, object, hashMap);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return hashMap;
+    }
+
+    /***
+     * 解析screen项
+     * @param key
+     * @param jsonObject
+     */
+    public static void parseScreenData(String key, JSONObject jsonObject, HashMap<String, String> hashMap) {
+        ConfigureEntity entity = null;
+        //读取第一屏的数据
+        Iterator<String> keys = jsonObject.keys();
+        try {
+            if (key == null) {
+                //读取第一屏screen的key;
+                while (keys.hasNext()) {
+                    key = keys.next();
+                }
+                jsonObject = jsonObject.getJSONObject(key);
+            }
+            keys = jsonObject.keys();
+            while (keys.hasNext()) {
+                String childKey = keys.next();
+                if (isItem(childKey)) {
+                    JSONObject object = jsonObject.getJSONObject(childKey);
+                    parseItemEntity(childKey, object, hashMap);
+                }else if ("value".equals(childKey)) {
+                    String value = jsonObject.optString(childKey);
+                    if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
+                        hashMap.put(key, value);
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    /***
+     * key是否属于item
+     * @param key
+     * @return
+     */
+    public static boolean isItem(String key) {
+        if ("type".equals(key)) {
+           return false;
+        } else if ("value".equals(key)) {
+            return false;
+        } else if ("summary".equals(key)) {
+            return false;
+        } else if ("title".equals(key)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /***
+     * 解析item项
+     * @param key
+     * @param jsonObject
+     */
+    public static void parseItemEntity(String key, JSONObject jsonObject, HashMap<String, String> hashMap) {
+        String type = jsonObject.optString(ConfigureManager.TYPE);
+        switch (type) {
+            case ConfigureType.SCREEN:
+                parseScreenData(key, jsonObject, hashMap);
+                break;
+        }
+        String value = null;
+        Object dot = jsonObject.opt(ConfigureManager.DEFAULT_VALUE);
+        Object vot = jsonObject.opt(ConfigureManager.VALUE);
+        if (dot != null) {
+            value = dot.toString();
+        }else if (vot != null){
+            value = vot.toString();
+
+        }
+        if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
+            hashMap.put(key, value);
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public static ConfigureEntity getScreen(String key) {
+        try {
+            JSONObject object = new JSONObject(Constants.jsonDate);
+            if (key == null) {
+                return parseData(null, object);
+            } else {
+                return  parseScreenData(null, key, object);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static ConfigureEntity parseScreenData(String key,String selectedKey, JSONObject jsonObject) {
+        ConfigureEntity entity = null;
+        //读取第一屏的数据
+        Iterator<String> keys = jsonObject.keys();
+        try {
+            if (key == null) {
+                //读取第一屏screen的key;
+                while (keys.hasNext()) {
+                    key = keys.next();
+                }
+                jsonObject = jsonObject.getJSONObject(key);
+            }
+            keys = jsonObject.keys();
+            boolean hasFinded = TextUtils.equals(key, selectedKey);
+            LinkedList<ConfigureEntity> linkedList = new LinkedList<>();
+            if (hasFinded) {
+                entity = new ConfigureScreenEntity();
+                entity.setKey(key);
+            }
+            while (keys.hasNext()) {
+                String childKey = keys.next();
+                String optString = jsonObject.optString(childKey);
+                if(hasFinded) {
+                   if (isItem(childKey)) {
+                       JSONObject object = jsonObject.getJSONObject(childKey);
+                       ConfigureEntity entity1 = parseEntity(childKey, object);
+                       linkedList.add(entity1);
+                   } else {
+                       setEntity(entity, childKey, optString);
+                   }
+                } else {
+                    if (isItem(childKey)) {
+                        JSONObject object = jsonObject.getJSONObject(childKey);
+                        String type = object.optString(ConfigureManager.TYPE);
+                        if (TextUtils.equals(ConfigureType.SCREEN, type)) {
+                           entity = parseScreenData(childKey, selectedKey, object);
+                            if (entity != null) {
+                                return entity;
+                            }
+                        }
+                    }
+                }
+            }
+            return entity;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void setEntity(ConfigureEntity entity,String key, String optString) {
+        if ("type".equals(key)) {
+            entity.setType(optString);
+        } else if ("value".equals(key)) {
+            entity.setValue(optString);
+        } else if ("summary".equals(key)) {
+            entity.setSummary(optString);
+        } else if ("title".equals(key)) {
+            entity.setTitle(optString);
+        }
+    }
+
+   /* *//***
+     * 解析item项
+     * @param key
+     * @param jsonObject
+     *//*
+    public static void parseItemEntity(String key, JSONObject jsonObject) {
+        String type = jsonObject.optString(ConfigureManager.TYPE);
+        switch (type) {
+            case ConfigureType.SCREEN:
+                parseScreenData(key, jsonObject);
+                break;
+        }
+
+    }*/
+
+
+
+
+
+
+
+
+
+
+
 }
