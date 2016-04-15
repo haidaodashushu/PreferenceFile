4a801359f73a9ae41ee7d71d56b0ebbbc1c1f1cb
w812618892 <w812618892@126.com>
2016-04-15 15:30:11 +0800

    修复switch添加[true,false]功能

--- app/src/main/java/com/example/wangzhengkui/preferencefile/entity/Constants.java
+++ app/src/main/java/com/example/wangzhengkui/preferencefile/entity/Constants.java
@@ -1,7 +1,7 @@
 package com.example.wangzhengkui.preferencefile.entity;
 
 public class Constants{
-		public static String jsonDate = "{\"lianjiefangshi\":{\"title\":\"其他链接方式\",\"type\":\"screen\",\"fly_mode\":{\"title\":\"飞行模式\",\"defaultValue\":\"true\",\"type\":\"switch\"},\"vpn\":{\"title\":\"VPN\",\"type\":\"screen\"},\"wangluoredian\":{\"title\":\"网络热点\",\"type\":\"screen\"},\"mi_manyou\":{\"title\":\"小米漫游\",\"type\":\"category\",\"global_manyou\":{\"title\":\"国际上网漫游\",\"summary\":\"可信赖的国际漫游\",\"type\":\"screen\"}},\"apn\":{\"title\":\"APN\",\"defaultValue\":\"cmnet\",\"type\":\"editor\"},\"gexingzhuti\":{\"title\":\"个性主题\",\"summary\":\"主题4\",\"type\":\"list\",\"entry\":[\"主题1\",\"主题2\",\"主题3\"]}}}";
+		public static String jsonDate = "{\"lianjiefangshi\":{\"title\":\"其他链接方式\",\"type\":\"screen\",\"fly_mode\":{\"entry\":[\"true\",\"false\"],\"title\":\"飞行模式\",\"defaultValue\":\"true\",\"value\":\"true\",\"type\":\"switch\"},\"vpn\":{\"title\":\"VPN\",\"type\":\"screen\"},\"wangluoredian\":{\"title\":\"网络热点\",\"type\":\"screen\"},\"mi_manyou\":{\"title\":\"小米漫游\",\"type\":\"category\",\"global_manyou\":{\"title\":\"国际上网漫游\",\"summary\":\"可信赖的国际漫游\",\"type\":\"screen\"}},\"apn\":{\"title\":\"APN\",\"defaultValue\":\"cmnet\",\"value\":\"cmnet\",\"type\":\"editor\"},\"gexingzhuti\":{\"title\":\"个性主题\",\"summary\":\"主题4\",\"value\":\"主题4\",\"type\":\"list\",\"entry\":[\"主题1\",\"主题2\",\"主题3\"]}}}";
 
 		public static String version="1";
 
